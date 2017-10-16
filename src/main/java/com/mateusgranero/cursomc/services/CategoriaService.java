d0fbf7edeee5d0e266f54d2c83fc2614683ea8e1
Mateus Granero <mateusgranero@gmail.com>
2017-10-16 01:16:44 -0200

    Criando repository e service para categoria

---
+++ src/main/java/com/mateusgranero/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.mateusgranero.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mateusgranero.cursomc.domain.Categoria;
+import com.mateusgranero.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo; 
+	
+	public Categoria buscar(Integer id) {
+		
+		Categoria obj = repo.findOne(id);
+		
+		return obj;
+		
+	}
+}
