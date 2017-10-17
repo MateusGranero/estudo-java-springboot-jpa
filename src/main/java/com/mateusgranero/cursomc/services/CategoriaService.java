ec86c47ab9fac2fd0c29251ef2e0cd0ca5ddde2b
Mateus Granero <mateusgranero@gmail.com>
2017-10-17 00:17:57 -0200

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/mateusgranero/cursomc/services/CategoriaService.java
+++ src/main/java/com/mateusgranero/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.mateusgranero.cursomc.domain.Categoria;
 import com.mateusgranero.cursomc.repositories.CategoriaRepository;
+import com.mateusgranero.cursomc.services.exeptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -15,7 +16,10 @@ public class CategoriaService {
 	public Categoria buscar(Integer id) {
 		
 		Categoria obj = repo.findOne(id);
-		
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: "+ Categoria.class.getName());
+		}
 		return obj;
 		
 	}
